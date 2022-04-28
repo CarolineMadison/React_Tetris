e7e772b017b1fabd779702bc0f20b7e4e53f6755
Caroline Brownlee <caroline.brownlee@evicore.com>
2022-04-27 21:22:52 -0500

    initial startup

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import './App.css';
+
+import GridBoard from './components/GridBoard'
 
 function App() {
   return (
@@ -6,6 +9,7 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
       </header>
+      <GridBoard />
     </div>
   );
 }
