1dd47aea65e82003d7064725b278a6a1114d26d5
Caroline Madison <50077163+CarolineBrownlee@users.noreply.github.com>
2022-04-28 06:11:45 -0500

    Initial set up (#4)
    * replaced lost code in files... grid board now showing
    
    * initial startup
    
    * added initial next block
    
    Co-authored-by: Caroline Brownlee <caroline.brownlee@evicore.com>

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -1,4 +1,8 @@
+import React from 'react';
 import './App.css';
+
+import GridBoard from './components/GridBoard'
+import NextBlock from './components/NextBlock'
 
 function App() {
   return (
@@ -6,6 +10,8 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
       </header>
+      <GridBoard />
+      <NextBlock />
     </div>
   );
 }
