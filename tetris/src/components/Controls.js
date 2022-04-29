d57cc13c0db024bdbd5a682afb63c071592ae9af
Caroline Brownlee <caroline.brownlee@evicore.com>
2022-04-29 14:16:57 -0500

    added controls

---
+++ tetris/src/components/Controls.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Controls(props) {
+    return (
+        <div className="controls">
+            {/* left */}
+            <button className="control-button" onClick={(e) => {
+
+            }}>Left</button>
+
+            {/* right */}
+            <button className="control-button" onClick={(e) => {
+
+            }}>Right</button>
+
+            {/* rotate */}
+            <button className="control-button" onClick={(e) => {
+
+            }}>Rotate</button>
+
+            {/* down */}
+            <button className="control-button" onClick={(e) => {
+
+            }}>Down</button>
+
+        </div>
+    )
+}
