ac76a00522b33c90dbb92d5bdc39f01a1cc9d658
Caroline Brownlee <brownleecaroline@gmail.com>
2022-05-09 06:30:10 -0500

    added connection for controls

--- tetris/src/components/Controls.js
+++ tetris/src/components/Controls.js
@@ -4,28 +4,32 @@
 //3. Add the controls to App.js
 
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
 
 export default function Controls(props) {
+    const dispatch = useDispatch()
+    const isRunning = useSelector((state) => state.isRunning)
     return (
         <div className="controls">
             {/* left */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(moveLeft())
             }}>Left</button>
 
             {/* right */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(moveRight())
             }}>Right</button>
 
             {/* rotate */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(rotate())
             }}>Rotate</button>
 
             {/* down */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(moveDown())
             }}>Down</button>
 
         </div>
