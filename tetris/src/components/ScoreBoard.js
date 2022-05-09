ffd4b71710bb81b134a34ed711f19b112a1ab32c
Caroline Brownlee <brownleecaroline@gmail.com>
2022-05-09 06:34:05 -0500

    added connection for score board

--- tetris/src/components/ScoreBoard.js
+++ tetris/src/components/ScoreBoard.js
@@ -5,15 +5,30 @@
 
 import React from 'react'
 
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { pause, resume, restart } from '../actions'
+
 export default function ScoreBoard(props) {
+    const dispatch = useDispatch()
+    const game = useSelector((state) => state.game)
+    const { score, isRunning, gameOver } = game
+
     return (
         <div className="score-board">
-            <div>Score:{ props.score }</div>
+            <div>Score:{ score }</div>
             <div>Level: 1</div>
             <button className="score-board-button" onClick={(e) => {
-            }}>Play</button>
+                if (gameOver) { return }
+                if (isRunning) {
+                    dispatch(pause())
+                } else {
+                    dispatch(resume())
+                }
+            }}>{isRunning ? 'Pause' : 'Play'}</button>
             <button className="score-board-button" onClick={(e) => {
-            }}>Restart</button>s
+                dispatch(restart())
+            }}>Restart</button>
         </div>
     )
 }
